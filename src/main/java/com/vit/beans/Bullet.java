10ba03bc592144bc44e1b04ab42714f0bec3a5db
sagar29chhatre <71561236+sagar29chhatre@users.noreply.github.com>
2021-01-29 07:42:31 -0800

    done with HIS-169

--- src/main/java/com/vit/beans/Bullet.java
+++ src/main/java/com/vit/beans/Bullet.java
@@ -2,6 +2,12 @@ package com.vit.beans;
 
 public class Bullet {
 
-	
-	
+	// HIS-169
+	//priority task(bugfix) that is assigned @12:20pm
+	void bugfix() {
+		// working on....
+		//...
+		//...
+		//done
+	}
 }
