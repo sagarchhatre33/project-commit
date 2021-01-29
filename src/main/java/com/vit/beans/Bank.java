8e19ba5b9a7f1f78281235ca5231d46ee2ea6180
sagar29chhatre <71561236+sagar29chhatre@users.noreply.github.com>
2021-01-28 22:43:15 -0800

    commited

--- src/main/java/com/vit/beans/Bank.java
+++ src/main/java/com/vit/beans/Bank.java
@@ -3,4 +3,11 @@ package com.vit.beans;
 public class Bank implements Serializable {
 
 	private String accNo;
+	
+	public void taskAt8am(){
+		
+		// changes for HIS-122 are in progress....
+		int i =300;
+	}
+	
 }
